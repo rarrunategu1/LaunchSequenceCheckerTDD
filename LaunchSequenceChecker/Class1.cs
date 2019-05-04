332aee10e01673354d23fbcc5d8bc9c27336f9f7
rarrunategu1 <parker.rose@ymail.com>
2019-05-04 11:43:12 -0400

    LaunchSequenceChecker - returns simple bool equaling to true

--- LaunchSequenceChecker/Class1.cs
+++ LaunchSequenceChecker/Class1.cs
@@ -8,5 +8,22 @@ namespace LaunchSequenceChecker
 {
     public class Class1
     {
+        public void Main(string[] args)
+        {   
+            //LaunchSequence.launchSequenceChecker()
+            //stage_1 == [1, 2]
+            //stage_2 == [10, 12]
+            //dragon == [11, 111]
+        }
+       
     }
+    public class LaunchSequence
+    {
+        public static bool launchSequenceChecker(string[] systemNames, int[] stepNumbers)
+        {
+            return true;
+
+        }
+    }
+    
 }
---
+++ LaunchSequenceChecker/ClassTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LaunchSequenceChecker
+{
+    [TestFixture]
+    public class ClassTest
+    {
+        [Test]
+        public void Returnbool()
+        {
+            string[] input = { "stage1" };
+            int[] input2 = { 5 };
+            var output = LaunchSequence.launchSequenceChecker(input, input2);
+            var expected = true;
+            Assert.AreEqual(output, expected);
+        }
+
+
+        
+
+    }
+}
